8961e78d9d848c4aa6784aff4976954d917e7e76
buildwithlou <buildwithlou@gmail.com>
2026-07-08 17:14:50 -0400

    Adding the list task function

--- task_tracker.py
+++ task_tracker.py
@@ -107,14 +107,32 @@ def update_task_status(task_id: int, new_status: str):
     print(f"Error: Task with ID {task_id} not found.")
 
 
-def list_tasks():
-"""Displays saved tasks in the terminal.
-If a status_filter is provided, displays only matching tasks."""
+def list_tasks(status_filter: str = None):
+    """Displays saved tasks in the terminal. If a status_filter is provided, displays only matching tasks."""
     tasks = load_tasks()
 
     if not tasks:
         print("No tasks found.")
         return
+
+    # Apply the filter if the user provided one (todo, in-progress, done)
+    if status_filter:
+        filtered_tasks = [task for task in tasks if task["status"] == status_filter]
+    else:
+        filtered_tasks = tasks
+
+    if not filtered_tasks:
+        print(f"No tasks found with status: '{status_filter}'")
+        return
+    if not filtered_tasks:
+        print(f"No tasks found with status: '{status_filter}'")
+        return
+
+    print(f"\n {'ID': <5} | {'Description': <40} | {'Status': <15}")
+    print("-" * 66)
+    for task in filtered_tasks:
+        print(f"{task['id']:<5} | {task['description']:<40} | {task['status']:<15}")
+    print()
 
 
 def main():
